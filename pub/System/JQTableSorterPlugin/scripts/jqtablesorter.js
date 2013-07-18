b6a1c761b6bd942ecf888877811ccd276c70b0c5
Sven Meyer <meyer@modell-aachen.de>
2013-07-18 13:32:16 +0200

    Bugfix bei leerem tbody-Tag

--- pub/System/JQTableSorterPlugin/scripts/jqtablesorter.js
+++ pub/System/JQTableSorterPlugin/scripts/jqtablesorter.js
@@ -1,18 +1,25 @@
-var pattern = /^[0-9]{1,2} [A-Za-z]{3,10} [0-9]{4}$/;
-$.tablesorter.addParser({
-  id: "qwikiDate",
-  is: function (s) {
-    return s.match(new RegExp(pattern));
-  }, format: function (s) {
-    return $.tablesorter.formatFloat(new Date(s).getTime());
-  }, type: "numeric"
-});
-
 (function($) {
+  $.tablesorter.addParser({
+    id: "qwikiDate",
+    is: function (s) {
+      var pattern = /^[0-9]{1,2} [A-Za-z]{3,10} [0-9]{4}$/;
+      return s.match(new RegExp(pattern));
+    }, format: function (s) {
+      return $.tablesorter.formatFloat(new Date(s).getTime());
+    }, type: "numeric"
+  });
+
   $(document).ready( function() {
     var sortableTables = $('table.tablesorter');
     $.each( sortableTables, function( index, table ) {
       var opts = $(table).metadata();
+
+      // Fix: jquery.tablesorter erzeugt Fehler bei leerem <tbody>
+      var body = $(table).find('tbody');
+      if ( $(body).html() == '' ) {
+        $(body).append('<tr><td style="display: none;"></td></tr>');
+      }
+
       if ( opts ) {
         $(table).tablesorter( opts );
       }
