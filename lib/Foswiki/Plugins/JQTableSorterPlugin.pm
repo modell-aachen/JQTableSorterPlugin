3ac594694ecc53a82e167d1ffc77483539362ae5
Sven Meyer <meyer@modell-aachen.de>
2013-08-08 10:28:45 +0200

    Release erhöht

--- lib/Foswiki/Plugins/JQTableSorterPlugin.pm
+++ lib/Foswiki/Plugins/JQTableSorterPlugin.pm
@@ -6,8 +6,8 @@ use warnings;
 use Foswiki::Func ();
 use Foswiki::Plugins ();
 
-our $VERSION = "1.0.1";
-our $RELEASE = "1.0.1";
+our $VERSION = "1.0.2";
+our $RELEASE = "1.0.2";
 our $NO_PREFS_IN_TOPIC = 1;
 our $SHORTDESCRIPTION = "Enables sorting functionality for Foswiki tables.";
 
