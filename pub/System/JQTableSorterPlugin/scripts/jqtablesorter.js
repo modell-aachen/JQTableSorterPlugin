84c63152ed93439d9310dc6fd059eda23ff23de6
Stephan Osthold <osthold@modell-aachen.de>
2016-01-28 09:22:54 +0100

    Feature: 'qwikiSize' sorter
    Ticket: #10166
    Release-Note(feature,appdev) {
      New type for tablesorter: qwikiSize for file sizes.
    [...]
      Will take SE/IEC prefixes into account. If there are two numbers in a
      string, the second will be chosen.
    }

--- pub/System/JQTableSorterPlugin/scripts/jqtablesorter.js
+++ pub/System/JQTableSorterPlugin/scripts/jqtablesorter.js
@@ -25,6 +25,46 @@
     type: "numeric"
   });
 
+  $.tablesorter.addParser({
+    id: "qwikiSize",
+    is: function (s) {
+      var size = null;
+      var match = /\d+(?:[ kmgtpKMGTP]|$)/.exec(s);
+      return match;
+    },
+    format: function (s) {
+      var size = null;
+      var match = /(?:.* )?(\d+)\s*([kmgtpKMGTP]?)((?:i?[bB])?)/.exec(s);
+      if(match) {
+          size = match[1];
+          var multi;
+          if(match[3].toLowerCase() === 'ib'){
+              // 1k == 2^10
+              multi = {
+                  k: 1024,
+                  m: 1048576,
+                  g: 1073741824,
+                  t: 1099511627776,
+                  p: 1125899906842624
+              };
+          } else {
+              // 1k == 10^3
+              multi = {
+                  k: 1000,
+                  m: 1000000,
+                  g: 1000000000,
+                  t: 1000000000000,
+                  p: 1000000000000000
+              };
+          };
+          var factor = multi[match[2].toLowerCase()];
+          if(factor !== undefined) size *= factor;
+      }
+      return size;
+    },
+    type: "numeric"
+  });
+
   $(document).ready( function() {
     var sortableTables = $('table.tablesorter, table.jqsortable');
     $(sortableTables).livequery( function() {
